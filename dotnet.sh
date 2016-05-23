d7d3c61066be2c30ce5906305bca5271d74037f9
Carl Winkler <carl@carlossus.com>
2016-05-23 09:37:43 +0800

    Add hashbang to dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
