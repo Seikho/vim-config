d13e8da29f43da7ed73517089d1c4cdd82ec1a64
Carl Winkler <carl@carlossus.com>
2016-09-01 11:19:42 +0800

    Add .../go/bin to PATH instead of creating symlinks

--- go.sh
+++ go.sh
@@ -6,13 +6,9 @@ sudo curl -s https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz >> $
 # Unpack the archive
 (cd $HOME && sudo tar -xvf go.tar.gz)
 
-# Enable 'go', 'gofmt' and 'godoc' from the command line
-sudo ln -s $HOME/go/bin/go /usr/bin/go
-sudo ln -s $HOME/go/bin/godoc /usr/bin/godoc
-sudo ln -s $HOME/go/bin/gofmt /usr/bin/gofmt
-
 # Create $GOPATH
 touch $HOME/.bashrc
+echo "export PATH=\"$HOME/go/bin:\$PATH\"" >> $HOME/.bashrc
 echo "export GOPATH=\"$HOME/gotools\"" >> $HOME/.bashrc
 echo "export GOROOT=\"$HOME/go\"" >> $HOME/.bashrc
 source $HOME/.bashrc
