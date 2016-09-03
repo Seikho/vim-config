588b70f035205960bcda5ec7891c7c1e3a993697
Carl <carl@carlossus.com>
2016-09-03 23:12:45 +0800

    Set GOPATH to projects/go

--- go.sh
+++ go.sh
@@ -4,11 +4,11 @@
 sudo curl -s https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz >> $HOME/go.tar.gz
 
 # Unpack the archive
-(cd $HOME && sudo tar -xvf go.tar.gz)
+(cd $HOME && sudo tar -xvf go.tar.gz && rm go.tar.gz)
 
-# Create $GOPATH
+# Allow Go from CLI and create $GOPATH and $GOROOT
 touch $HOME/.bashrc
 echo "export PATH=\"$HOME/go/bin:\$PATH\"" >> $HOME/.bashrc
-echo "export GOPATH=\"$HOME/gotools\"" >> $HOME/.bashrc
+echo "export GOPATH=\"$HOME/projects/go\"" >> $HOME/.bashrc
 echo "export GOROOT=\"$HOME/go\"" >> $HOME/.bashrc
 source $HOME/.bashrc
