1fbf3fff23184ada9f0ec8ecbae3a53d0f3a6e9e
Carl Winkler <carl@winkler.id.au>
2017-05-27 23:01:49 +0800

    Remove YCM and add osx install

---
+++ osx-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install tools for YouCompleteMe
+# sudo apt-get install build-essential python-dev cmake -y
+
+# Create vim bundle/ftplugin folders and parent directories
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/ftplugin
+
+# Get Vundle
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+
+# Get Vim Colorschemes prior to running Vim
+git clone https://github.com/flazz/vim-colorschemes ~/.vim/bundle/vim-colorschemes
+
+# Create a symbolic link of .vimrc in home directory
+ln -s $PWD/.vimrc ~/.vimrc
+
+# Create a symbolic link of typescript.vim in filetype plugins
+ln -s $PWD/typescript.vim ~/.vim/ftplugin/typescript.vim
+
+# Install Vundle plugins
+vim +PluginInstall +qall
+
+# Create a symbolic link of typescript_filter.vim in NERDTree filetype plugins
+ln -s $PWD/typescript_filter.vim ~/.vim/bundle/nerdtree/nerdtree_plugin/typescript_filter.vim
+
+# Install Vimproc
+(cd ~/.vim/bundle/vimproc.vim && make)
