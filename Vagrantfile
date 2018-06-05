fdf8bd9eaf26d831b5b0136cf83b35708419070d
Carl Winkler <carl@carlossus.com>
2018-06-05 14:49:20 +0800

    Upgrade to ubuntu 16.04, node 8, docker ce 18

--- Vagrantfile
+++ Vagrantfile
@@ -2,7 +2,7 @@
 
 Vagrant.configure(2) do |config|
 
-  config.vm.box = "phusion/ubuntu-14.04-amd64"
+  config.vm.box = "hashicorp-vagrant/ubuntu-16.04"
   config.vm.network "private_network", ip: "192.168.33.10"
   config.vm.synced_folder "..", "/home/vagrant/projects"
 
@@ -15,8 +15,5 @@ Vagrant.configure(2) do |config|
   # Install Docker v1.x and add user to docker group
   config.vm.provision "shell", path: "docker.sh"
   config.vm.provision "shell", inline: "sudo usermod -a -G docker vagrant"
-
-  # Install Go 1.7
-  config.vm.provision "shell", path: "go.sh"
 
 end
--- docker.sh
+++ docker.sh
@@ -5,4 +5,20 @@ sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /
 
 sudo apt-get update
 
-sudo apt-get install docker-engine -y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce -y
